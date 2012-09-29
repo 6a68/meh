dcdef0cae62d655ae12cf7b64763d862bc0551f1
Jared Hirsch <ohai@6a68.net>
2012-09-28 18:41:00 -0700

    add test for creating primary 123done user. buggy for now.

--- new_user_secondary_woah.js
+++ new_user_secondary_woah.js
@@ -21,8 +21,18 @@ require('./lib/wd-extensions.js');
 // generate a randome email we'll use
 const theEmail = restmail.randomEmail(10);
 var browser = wd.remote();
+var eyedeemail = restmail.randomEmail(10, 'eyedee.me');
+function startup(cb) {
+  browser.chain()
+    .newSession()
+    .get(persona_urls["123done"])
+    .waitForDisplayed(CSS["123done.org"].signinButton, function(err, el) {
+      browser.clickElement(el, cb)
+    })
+}
 
 vowsHarness({
+
   "create a new selenium session": function(done) {
     browser.newSession(done);
   },
@@ -59,5 +69,46 @@ vowsHarness({
   },
   "shut down": function(done) {
     browser.quit(done);
-  }
+  },
+
+
+  "startup again, load 123done, click sign in": function(done) {
+    startup(done)
+  },
+  "sign in a new eyedeemee user": function(done) {
+    function noop() {}
+    browser.chain()
+      .waitForWindow(CSS['persona.org'].windowName)
+      .elementByCss(CSS['dialog'].emailInput, function(err, elem) {
+        browser.type(elem, eyedeemail, noop);
+      })
+      .elementByCss(CSS['dialog'].newEmailNextButton, function(err, elem) {
+        browser.clickElement(elem, noop);
+      })
+      // TODO regular wait doesn't seem to be working.
+      // need to ensure there's no spinner inside the button, maybe?
+      .waitForDisplayed(CSS['dialog'].verifyWithPrimaryButton)
+      .elementByCss(CSS['dialog'].verifyWithPrimaryButton, function(err, elem) {
+        browser.clickElement(elem, noop);
+      })
+      .waitForDisplayed(CSS['eyedee.me'].newPassword)
+      .elementByCss(CSS['eyedee.me'].newPassword, function(err, elem) {
+        browser.type(elem, eyedeemail.split('@')[0], noop);
+      })
+      .elementByCss(CSS['eyedee.me'].createAccountButton, function(err, elem) {
+        browser.clickElement(elem, done);
+      })
+    },
+    // TODO 123done never seems to log in. something up with beta server?
+    "switch back to main window and verify we're logged in": function(done) {
+      // this part copied from one of the sign in tests :-P
+      browser.windowHandles(function(err, handles) {
+        browser.window(handles[0], function(err) {
+          browser.waitForElementText(CSS['123done.org'].currentlyLoggedInEmail, function(err, text) {
+            assert.equal(text, eyedeemail);
+            done()
+          });
+        });
+      });
+    }
 }, module);
